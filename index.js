8fa121d590d5d3845997187cd246d45ac4647a68
parvejme24 <mdparvejme24@gmail.com>
2024-03-23 01:47:25 +0600

    update index.js file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.get("/product", (req, res) => {
+  res.send("This is pproduct pae");
 });
 
 app.listen(PORT, () => {
